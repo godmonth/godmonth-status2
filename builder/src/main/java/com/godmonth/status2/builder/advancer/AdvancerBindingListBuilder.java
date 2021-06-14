810c97f5d9fe809937ccd6301680a0c1f7fe91d1
godmonth <link2people@gmail.com>
2021-06-14 11:00:29 +0800

    update AdvancerBindingListBuilder

--- builder/src/main/java/com/godmonth/status2/builder/advancer/AdvancerBindingListBuilder.java
+++ builder/src/main/java/com/godmonth/status2/builder/advancer/AdvancerBindingListBuilder.java
@@ -6,6 +6,7 @@ import com.godmonth.status2.builder.binding.BindingListBuilder;
 import lombok.Builder;
 import lombok.Singular;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.Validate;
 import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 
@@ -14,6 +15,7 @@ import java.util.List;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 /**
  * <p></p >
@@ -25,7 +27,10 @@ public class AdvancerBindingListBuilder {
 
     @Builder
     private static List<Pair<Object, StatusAdvancer>> build(ClassLoader classLoader, @Singular Set<String> packageNames, Class modelClass, Predicate<Class> predicate, AutowireCapableBeanFactory autowireCapableBeanFactory, Function<Class, Object[]> bindingKeyFunction) throws IOException, ClassNotFoundException {
-        return BindingListBuilder.<StatusAdvancer>builder().classLoader(classLoader).enableAnnotationClass(Advancer.class).packageNames(packageNames).ancestorClass(StatusAdvancer.class).modelClass(modelClass).predicate(predicate).autowireCapableBeanFactory(autowireCapableBeanFactory).bindingKeyFunction(bindingKeyFunction).build();
+        final List<Pair<Object, StatusAdvancer>> advancer = BindingListBuilder.<StatusAdvancer>builder().classLoader(classLoader).enableAnnotationClass(Advancer.class).packageNames(packageNames).ancestorClass(StatusAdvancer.class).modelClass(modelClass).predicate(predicate).autowireCapableBeanFactory(autowireCapableBeanFactory).bindingKeyFunction(bindingKeyFunction).build();
+        final long count = advancer.stream().map(Pair::getLeft).distinct().count();
+        Validate.isTrue(count == advancer.size(), "advancer keys are duplicated.%s", advancer.stream().map(Pair::getLeft).collect(Collectors.toList()));
+        return advancer;
     }
 
 
