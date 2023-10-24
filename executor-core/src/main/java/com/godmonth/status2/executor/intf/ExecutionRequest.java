44f5e34f8d72b07354ba3dec38af16a75ab11e1f
godmonth <link2people@gmail.com>
2023-10-24 17:13:05 +0800

    add constructor

--- executor-core/src/main/java/com/godmonth/status2/executor/intf/ExecutionRequest.java
+++ executor-core/src/main/java/com/godmonth/status2/executor/intf/ExecutionRequest.java
@@ -1,7 +1,9 @@
 package com.godmonth.status2.executor.intf;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * <p></p >
@@ -10,10 +12,16 @@ import lombok.Data;
  */
 @Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ExecutionRequest<MODEL, INST> {
     private MODEL model;
 
     private INST instruction;
 
     private Object message;
+
+    public ExecutionRequest(MODEL model) {
+        this.model = model;
+    }
 }
